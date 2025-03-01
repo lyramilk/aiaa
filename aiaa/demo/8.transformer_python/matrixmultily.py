d56ceaa4e65da68da233862f9307fad187f0187a
lyramilk <lyramilk@qq.com>
2025-03-01 19:53:59 +0800

    矩阵相乘

--- aiaa/demo/8.transformer_python/matrixmultily.py
+++ aiaa/demo/8.transformer_python/matrixmultily.py
@@ -9,7 +9,9 @@ torch.manual_seed(1)
 class MatrixMultiplication:
     def __init__(self, W):
         self.W = W
+        print("Matrix. W.shape",self.W.shape,",W.device",self.W.device)
 
+    @torch.inference_mode()
     def multiply(self, input):
         return input @ self.W
 
@@ -19,10 +21,10 @@ class SVGMatrixMultiplication:
         U, S, V = torch.svd(W)
         self.A = U[:, :r] @ torch.diag(torch.sqrt(S[:r]))
         self.B = torch.diag(torch.sqrt(S[:r])) @ V[:, :r].T
-        print("SVG. A.shape",self.A.shape)
-        print("SVG. B.shape",self.B.shape)
+        print("SVG. A.shape",self.A.shape,",A.device",self.A.device)
+        print("SVG. B.shape",self.B.shape,",B.device",self.B.device)
 
-
+    @torch.inference_mode()
     def multiply(self, input):
         bx =  input @ self.A
         return bx @ self.B
@@ -35,11 +37,11 @@ class LoRAMatrixMultiplication:
         self.W = W
         self.A = torch.rand(self.n_input, r, requires_grad=True)
         self.B = torch.rand(r, self.n_output, requires_grad=True)
-        print("LoRA. A.shape",self.A.shape)
-        print("LoRA. B.shape",self.B.shape)
+        print("LoRA. A.shape",self.A.shape,",A.device",self.A.device)
+        print("LoRA. B.shape",self.B.shape,",B.device",self.B.device)
         self.train(0.001,10000,0.1);
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def multiply(self, input):
         bx =  input @ self.A
         return bx @ self.B
@@ -76,46 +78,51 @@ def cos_similarity(a,b):
 # 定义矩阵和向量的维度
 n_input = 10000
 n_output = 2000
-r = 10  # 低秩矩阵的秩
+r = 1  # 低秩矩阵的秩
 # 初始化原始矩阵 W
 W = torch.rand(n_input, n_output)
 
 
-new_input = torch.rand(1,n_input)
-print("W.shape",W.shape)
-print("new_input.shape",new_input.shape)
-
-
-# 直接矩阵乘法
 w1 = MatrixMultiplication(W)
-start_time = time.time()
-y_direct = w1.multiply(new_input)
-end_time = time.time()
-direct_time = end_time - start_time
-print(f"直接矩阵乘法耗时: {direct_time} 秒")
-print(f"直接矩阵乘法结果: {y_direct.mean()}")
-print(f"输出的形状: {y_direct.shape}")
-
-
-# SVG 分解后的矩阵乘法
 w2 = SVGMatrixMultiplication(W, r)
-start_time = time.time()
-y_svg = w2.multiply(new_input)
-end_time = time.time()
-svg_time = end_time - start_time
-print(f"SVG 分解后的矩阵乘法耗时: {svg_time} 秒")
-print(f"SVG 分解后的矩阵乘法结果: {y_svg.mean()}")
-print(f"结果是否接近: {torch.allclose(y_direct, y_svg,rtol=0.01)}") # 允许1%的误差
-print(f"余弦相似度: {cos_similarity(y_direct, y_svg)}")
-
-
-# LoRA 分解后的矩阵乘法
 w3 = LoRAMatrixMultiplication(W, r)
-start_time = time.time()
-y_lora = w3.multiply(new_input)
-end_time = time.time()
-lora_time = end_time - start_time
-print(f"LoRA 分解后的矩阵乘法耗时: {lora_time} 秒")
-print(f"LoRA 分解后的矩阵乘法结果: {y_lora.mean()}")
-print(f"结果是否接近: {torch.allclose(y_direct, y_lora,rtol=0.01)}") # 允许1%的误差
-print(f"余弦相似度: {cos_similarity(y_direct, y_lora)}")
+
+for i in range(1):
+    print(f"第{i}次测试================================================================================")
+    new_input = torch.rand(1,n_input)
+    print("W.shape",W.shape)
+    print("new_input.shape",new_input.shape)
+
+
+    # 直接矩阵乘法
+    start_time = time.time()
+    y_direct = w1.multiply(new_input)
+    end_time = time.time()
+    direct_time = end_time - start_time
+    print(f"直接矩阵乘法耗时: {direct_time} 秒")
+    print(f"直接矩阵乘法结果: {y_direct.mean()}")
+    print(f"输出的形状: {y_direct.shape}")
+
+
+    # SVG 分解后的矩阵乘法
+    start_time = time.time()
+    y_svg = w2.multiply(new_input)
+    end_time = time.time()
+    svg_time = end_time - start_time
+    print(f"SVG 分解后的矩阵乘法耗时: {svg_time} 秒")
+    print(f"SVG 分解后的矩阵乘法结果: {y_svg.mean()}")
+    print(f"结果是否接近: {torch.allclose(y_direct, y_svg,rtol=0.02)}") # 允许2%的误差
+    print(f"余弦相似度: {cos_similarity(y_direct, y_svg)}")
+    print(f"速度比: {direct_time/svg_time}")
+
+
+    # LoRA 分解后的矩阵乘法
+    start_time = time.time()
+    y_lora = w3.multiply(new_input)
+    end_time = time.time()
+    lora_time = end_time - start_time
+    print(f"LoRA 分解后的矩阵乘法耗时: {lora_time} 秒")
+    print(f"LoRA 分解后的矩阵乘法结果: {y_lora.mean()}")
+    print(f"结果是否接近: {torch.allclose(y_direct, y_lora,rtol=0.02)}") # 允许2%的误差
+    print(f"余弦相似度: {cos_similarity(y_direct, y_lora)}")
+    print(f"速度比: {direct_time/lora_time}")
